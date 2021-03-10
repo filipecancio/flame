3102489f7d144950181e6fa848c89def1d5d6f05
cancio <filipe.cancio@outlook.com>
2021-03-10 20:31:55 -0300

    #1 Setting eviroment

---
+++ src/components/Flame/index.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Index: React.FC = props => {
+      return (
+        <>
+            <h1>It's a flame</h1>
+        </>
+      );
+    }
+
+export default Index;
